4d948aaadfecac4897bb9f7a0d25f94a0d86a7dd
Bulat <darts13579@gmail.com>
2020-09-08 18:25:29 +0300

    Migration for Books

--- Library/Migrations/20200908152357_AddedBooks.Designer.cs
+++ Library/Migrations/20200908152357_AddedBooks.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Library.Data.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace Library.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200908152357_AddedBooks")]
+    partial class AddedBooks
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
